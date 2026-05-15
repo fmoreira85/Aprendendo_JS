b54dca062e8d6c34f60b90ccfb721806e18a5852
fmoreira85 <fabiomoreiradacunha1@gmail.com>
2026-05-15 10:00:28 -0400

    exercicios

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const login = require("./auth");
 const produtos = require("./produtos")
+const calcular = require("./Calc")
 
 console.log(login("admin","123"));
 console.log(produtos);
+console.log(calcular.soma(9,5));
+console.log(calcular.sub(10,5));
